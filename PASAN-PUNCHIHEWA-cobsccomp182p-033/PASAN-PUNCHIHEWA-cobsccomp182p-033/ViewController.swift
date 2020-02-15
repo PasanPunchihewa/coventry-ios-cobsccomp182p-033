dc56094aa8e28d18ebd14d31feea7fa5e06c986d
PasanPunchihewa <33536279+PasanPunchihewa@users.noreply.github.com>
2020-02-15 12:26:16 +0530

    Add files via upload
    1st changes

---
+++ PASAN-PUNCHIHEWA-cobsccomp182p-033/PASAN-PUNCHIHEWA-cobsccomp182p-033/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PASAN-PUNCHIHEWA-cobsccomp182p-033
+//
+//  Created by JOHN DOE on /15/220.
+//  Copyright © 2020 PASAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PASAN-PUNCHIHEWA-cobsccomp182p-033/PASAN-PUNCHIHEWA-cobsccomp182p-033Tests/PASAN_PUNCHIHEWA_cobsccomp182p_033Tests.swift
@@ -0,0 +1,34 @@
+//
+//  PASAN_PUNCHIHEWA_cobsccomp182p_033Tests.swift
+//  PASAN-PUNCHIHEWA-cobsccomp182p-033Tests
+//
+//  Created by JOHN DOE on /15/220.
+//  Copyright © 2020 PASAN. All rights reserved.
+//
+
+import XCTest
+@testable import PASAN_PUNCHIHEWA_cobsccomp182p_033
+
+class PASAN_PUNCHIHEWA_cobsccomp182p_033Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
